109dfc689ebdfcc8fe81a4cde88baa9d377697b7
alexhlv <alexhlv@gmail.com>
2020-07-03 23:26:53 +0300

    Added 3 more banks with different balances. Added the ability to disable the bank

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,10 +1,23 @@
 const MONOBNANK = 'monobank';
 const PRIVATBANK = 'privat24';
 const MEGABANK = 'megabank';
+const PUMB = 'pumb'; 
+const OSCHADBANK = 'oschadbank'; 
+const AVAL = 'aval';
 
 let MONOBNANK_BALANCE = 100;
 let PRIVATBANK_BALANCE = 1000;
 let MEGABANK_BALANCE = -1000;
+let PUMB_BALANCE = 500; 
+let OSCHADBANK_BALANCE = 120; 
+let AVAL_BALANCE = 70;
+
+let IS_MONOBNANK_ENABLED = true;
+let IS_PRIVATBANK_ENABLED = true;
+let IS_MEGABANK_ENABLED = true;
+let IS_PUMB_ENABLED = true;
+let IS_OSCHADBANK_ENABLED = true;
+let IS_AVAL_ENABLED = true;
 
 let userBank = null;
 
@@ -19,6 +32,18 @@ function getBalance(bankName) {
 
     if (bankName === MEGABANK) {
         return MEGABANK_BALANCE;
+    }
+
+    if (bankName === PUMB) {
+        return PUMB_BALANCE;
+    }
+
+    if (bankName === OSCHADBANK) {
+        return OSCHADBANK_BALANCE;
+    }
+
+    if (bankName === AVAL) {
+        return AVAL_BALANCE;
     }
 }
 
@@ -37,6 +62,24 @@ function getMoney(bankName, money) {
 
     if (bankName === MEGABANK) {
         MEGABANK_BALANCE -= money;
+        
+        return money;
+    }
+
+    if (bankName === PUMB) {
+        PUMB_BALANCE -= money;
+        
+        return money;
+    }
+
+    if (bankName === OSCHADBANK) {
+        OSCHADBANK_BALANCE -= money;
+        
+        return money;
+    }
+
+    if (bankName === AVAL) {
+        AVAL_BALANCE -= money;
         
         return money;
     }
@@ -60,6 +103,24 @@ function addMoney(bankName, money) {
         
         return MEGABANK_BALANCE;
     }
+
+    if (bankName === PUMB) {
+        PUMB_BALANCE += money;
+        
+        return PUMB_BALANCE;
+    }
+
+    if (bankName === OSCHADBANK) {
+        OSCHADBANK_BALANCE += money;
+        
+        return OSCHADBANK_BALANCE;
+    }
+
+    if (bankName === AVAL) {
+        AVAL_BALANCE += money;
+        
+        return AVAL_BALANCE;
+    }
 }
 
 function chooseBank(expectedBankName) {
@@ -67,24 +128,58 @@ function chooseBank(expectedBankName) {
 
     return currentBank;
 }
+// 1
 
+
+
+// 2
 function getBank() {
-    const isMonobank = chooseBank(MONOBNANK); 
+    if (IS_MONOBNANK_ENABLED) {
+        const isMonobank = chooseBank(MONOBNANK); 
     
-    if (isMonobank) {
-        return MONOBNANK;
+        if (isMonobank) {
+            return MONOBNANK;
+        }
     }
-
-    const isPrivatBank = chooseBank(PRIVATBANK); 
     
-    if (isPrivatBank) {
-        return PRIVATBANK;
+    if (IS_PRIVATBANK_ENABLED) {
+        const isPrivatBank = chooseBank(PRIVATBANK); 
+    
+        if (isPrivatBank) {
+            return PRIVATBANK;
+        }
     }
-
-    const isMegabank = chooseBank(MEGABANK); 
     
-    if (isMegabank) {
-        return MEGABANK;
+    if (IS_MEGABANK_ENABLED) {
+        const isMegabank = chooseBank(MEGABANK); 
+    
+        if (isMegabank) {
+            return MEGABANK;
+        }
+    }
+    
+    if (IS_PUMB_ENABLED) {
+        const isPumb = chooseBank(PUMB); 
+    
+        if (isPumb) {
+            return PUMB;
+        }
+    }
+    
+    if (IS_OSCHADBANK_ENABLED) {
+        const isOschadbank = chooseBank(OSCHADBANK); 
+    
+        if (isOschadbank) {
+            return OSCHADBANK;
+        }
+    }
+    
+    if (IS_AVAL_ENABLED) {
+        const isAval = chooseBank(AVAL); 
+    
+        if (isAval) {
+            return AVAL;
+        }
     }
 
     return null;
@@ -132,6 +227,19 @@ function askToGetMoney() {
     } else {
         alert('Недостаточно средств');
     }
+}
+
+function deactivate() {
+    switch(userBank) {
+        case MONOBNANK: IS_MONOBNANK_ENABLED = false; break;
+        case PRIVATBANK: IS_PRIVATBANK_ENABLED = false; break;
+        case MEGABANK: IS_MEGABANK_ENABLED = false; break;
+        case PUMB: IS_PUMB_ENABLED = false; break;
+        case OSCHADBANK: IS_OSCHADBANK_ENABLED = false; break;
+        case AVAL: IS_AVAL_ENABLED = false; break;
+    }
+
+    chooseBankAccount();
 }
 
 chooseBankAccount();
